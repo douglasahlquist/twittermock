b849fe088c298c8d7aa325912718faa5143cba1f
Douglas Ahlquist <dahlquist@twitter.com>
2020-01-08 14:00:26 -0800

    Initial Commit

---
+++ TwitterMock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterMock
+//
+//  Created by Douglas Ahlquist on 1/8/20.
+//  Copyright © 2020 Twitter Next. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
